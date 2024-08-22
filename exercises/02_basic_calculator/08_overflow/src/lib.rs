3e7c9c84f0127801deb562e38bbb5292f88863df
Rakuram <rakuram.e96@gmail.com>
2024-08-22 08:23:17 +0530

    update solutions

--- exercises/02_basic_calculator/08_overflow/src/lib.rs
+++ exercises/02_basic_calculator/08_overflow/src/lib.rs
@@ -8,7 +8,8 @@
 pub fn factorial(n: u32) -> u32 {
     let mut result: u32 = 1;
     for i in 1..=n {
-        result = result.wrapping_mul(i.try_into().unwrap());
+        // result *= i;
+        result = result.wrapping_mul(i);
     }
     result
 }
